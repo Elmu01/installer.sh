49ccefc05f6dacedd53ab92aa9cbbf231ae7de2c
Elmu01 <emppu.niskanen1@gmail.com>
2022-04-11 01:43:42 +0300

    move some files and add more apps

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #update system
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 #update nvidia-drivers
-sudo pacman -S nvidia
+sudo pacman -S nvidia --noconfirm
 
 #Apps from pacman
-sudo pacman -S yay zsh bashtop bitwarden discord steam-native-runtime grub-customizer solaar 
+sudo pacman -S yay zsh bashtop bitwarden discord steam grub-customizer solaar lutris --noconfirm
 
 #apss from yay
-yay -S brave-bin tidal-hifi-bin octopi
+yay -S brave-bin tidal-hifi-bin octopi noisetorch-bin authy --noconfirm
 
