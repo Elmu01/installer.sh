21c28bca650d5bb62f729c8f5ecf008ac21a88ad
Elmu01 <46714220+Elmu01@users.noreply.github.com>
2022-04-10 23:03:25 +0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#update system
+sudo pacman -Syu
+
+#update nvidia-drivers
+Sudo pacman -S nvidia
+
+#YAY
+Sudo pacman -S yay
+
+#zsh shell
+yay -S zsh
+
+#Tidal-hifi
+yay -S tidal-hifi-bin
+
+#steam
+yay -S steam-native-runtime
