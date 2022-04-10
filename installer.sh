d4119ad67562fbf1c4383bec88d6c7e0fa3374bb
Elmu01 <emppu.niskanen1@gmail.com>
2022-04-11 00:48:15 +0300

    More simple

--- installer.sh
+++ installer.sh
@@ -7,25 +7,5 @@ sudo pacman -Syu
 Sudo pacman -S nvidia
 
 #YAY
-Sudo pacman -S yay
+Sudo pacman -S yay zsh brave-bin bitwarden discord tidal-hifi-bin steam-native-runtime
 
-#zsh shell
-yay -S zsh
-
-#brave-bin
-yay -S brave-bin
-
-#bitwarden
-yay -S bitwarden
-
-#authy atenthicator
-yay -S authy
-
-#Discord
-yay -S discord
-
-#Tidal-hifi
-yay -S tidal-hifi-bin
-
-#steam
-yay -S steam-native-runtime
