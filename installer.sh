7845b49eb276c50677bbf894bca5a9e99f786200
Elmu01 <emppu.niskanen1@gmail.com>
2022-04-11 02:10:28 +0300

    add some apps and --needed

--- installer.sh
+++ installer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #update system
-sudo pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm 
 
 #update nvidia-drivers
-sudo pacman -S nvidia --noconfirm
+sudo pacman -S nvidia --noconfirm --needed
 
 #Apps from pacman
-sudo pacman -S yay zsh bashtop bitwarden discord steam grub-customizer solaar lutris --noconfirm
+sudo pacman -S yay zsh bashtop bitwarden discord steam grub-customizer solaar lutris ksysguard --noconfirm --needed
 
 #apss from yay
-yay -S brave-bin tidal-hifi-bin octopi noisetorch-bin authy --noconfirm
+yay -S brave-bin tidal-hifi-bin octopi noisetorch-bin authy nvtop-git --noconfirm --needed
 
 
 
