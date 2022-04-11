f7079141abdc0d0224b8fb78f2a1742e45a9087e
Elmu01 <emppu.niskanen1@gmail.com>
2022-04-11 03:42:28 +0300

    add plank dock

--- installer.sh
+++ installer.sh
@@ -31,7 +31,8 @@ sudo pacman -S nvidia --noconfirm --needed
 sudo pacman -S yay zsh bashtop bitwarden discord steam grub-customizer solaar lutris ksysguard --noconfirm --needed
 
 #apss from yay
-yay -S brave-bin tidal-hifi-bin octopi noisetorch-bin authy nvtop-git --noconfirm --needed
+yay -S brave-bin tidal-hifi-bin octopi noisetorch-bin authy nvtop-git plank --noconfirm --needed
+
 
 #change bash to zsh
 chsh -s /bin/zsh
