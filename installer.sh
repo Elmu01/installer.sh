99beb10c0d71460387ff197ee9f673c03e312ddc
Elmu01 <46714220+Elmu01@users.noreply.github.com>
2023-11-01 21:47:51 +0200

    Changed AMD drivers installation

--- installer.sh
+++ installer.sh
@@ -19,7 +19,7 @@ echo -e "$BANNER"
 sudo pacman -Syu --noconfirm 
 
 #installs the programs from pacman
-sudo pacman -S amdgpu-pro-installer git zsh os-prober btop bitwarden discord steam grub-customizer solaar lutris plank gnome-boxes signal-desktop noto-fonts-emoji ksysguard flatpak starship --noconfirm --needed
+sudo pacman -S mesa vulkan-radeon libva-mesa-driver mesa-vdpau lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-mesa-vdpau git zsh os-prober btop bitwarden discord steam grub-customizer solaar lutris plank gnome-boxes signal-desktop noto-fonts-emoji ksysguard flatpak starship davinci-resolve --noconfirm --needed
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -31,7 +31,7 @@ rm -rf yay
 yay -S spotify brave-bin authy qdirstat appimagelauncher heroic-games-launcher cava piavpn-bin plex-desktop latte-dock-git --noconfirm --needed
 
 #installs the program from flatpak
-flatpak install -y flathub io.github.Foldex.AdwSteamGtk flathub net.davidotek.pupgui2  
+flatpak install -y flathub io.github.Foldex.AdwSteamGtk flathub net.davidotek.pupgui2 flatpak flathub sh.cider.Cider flatpak flathub net.rpcs3.RPCS3
 
 #starship preset
 echo 'eval "$(starship init zsh)"' > ~/.zshrc
