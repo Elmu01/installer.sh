dc6496a6142bb1d8c3767803c87a939411e25342
Elmu01 <46714220+Elmu01@users.noreply.github.com>
2023-11-07 03:37:29 +0200

    Script can automatically send "wallpaper" from github to specific folder

--- installer.sh
+++ installer.sh
@@ -16,6 +16,9 @@ echo -e "$BANNER"
 #update system
 sudo pacman -Syu --noconfirm 
 
+#get my wallpapers folder from Github to Documents folder
+git clone https://github.com/Elmu01/installer.sh.git --branch main --single-branch /home/elmu/Documents/Wallpapers
+
 #installs the programs from pacman
 sudo pacman -S mesa vulkan-radeon libva-mesa-driver mesa-vdpau lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-mesa-vdpau git zsh os-prober btop bitwarden discord steam grub-customizer solaar piper libreoffice-still lutris plank gnome-boxes signal-desktop noto-fonts-emoji ksysguard flatpak starship --noconfirm --needed
 
