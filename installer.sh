19c89d460bfb842896b7ecfaa9689731f6298d1b
Elmu01 <46714220+Elmu01@users.noreply.github.com>
2024-01-08 14:59:10 +0200

    Script set Mangohud and Neofetch config in the right place

--- installer.sh
+++ installer.sh
@@ -16,10 +16,15 @@ echo -e "$BANNER"
 #update system
 sudo pacman -Syu --noconfirm 
 
-#get my wallpapers folder from Github to Documents folder
+cd $HOME/Documents
 git clone https://github.com/Elmu01/installer.sh.git installer
-mv installer/Wallpapers $HOME/Documents/
-rm -rf installer
+ln -s $HOME/Documents/installer/Wallpapers $HOME/Documents/Wallpapers
+mkdir -p $HOME/.config/MangoHud
+ln -s $HOME/Documents/installer/MangoHud.conf $HOME/.config/MangoHud/MangoHud.conf
+ln -s $HOME/Documents/installer/neofetch $HOME/.config/neofetch
+cd installer
+git remote set-url origin git@github.com:Elmu01/installer.sh.git
+cd ../..
 
 #installs the programs from pacman
 sudo pacman -S mesa vulkan-radeon libva-mesa-driver mesa-vdpau lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-mesa-vdpau git zsh os-prober btop bitwarden discord steam grub-customizer solaar piper libreoffice-still lutris plank gnome-boxes signal-desktop noto-fonts-emoji ksysguard code flatpak starship --noconfirm --needed
@@ -43,6 +48,7 @@ lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjp
 sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
 ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+
 
 #starship preset
 echo 'eval "$(starship init zsh)"' > ~/.zshrc
